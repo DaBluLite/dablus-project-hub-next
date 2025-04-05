6a65a50a732201a6cea410de1dcd42c541714805
DaBluLite <dablulite@gmail.com>
2025-04-06 00:21:29 +0300

    a

--- src/app/api/auth/getuser/route.ts
+++ src/app/api/auth/getuser/route.ts
@@ -1,31 +1,36 @@
 import { auth } from "@/auth"
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
-export const POST = auth(async function POST(req) {
-    const json: {
-        redirect_url?: string
-    } | null = await req.json();
-    if(json && json.redirect_url) {
-        if (req.auth && req.auth.user) {
-            fetch(json.redirect_url, {
-                body: JSON.stringify({ username: req.auth.user.name, image: req.auth.user.image, status: 200 }),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                method: "POST"
-            })
-            return NextResponse.json({ username: req.auth.user.name, image: req.auth.user.image,  })
-        } else {
-            fetch(json.redirect_url, {
-                body: JSON.stringify({ message: "Not authenticated", status: 401 }),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                method: "POST",
-            })
-            return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
-        }
-    } else {
-        return NextResponse.json({ message: "Invalid request body" }, { status: 401 });
-    }
-})
+export function POST(req: NextRequest, ctx: never) {
+    return auth(async function POST(req) {
+        return await req.json().then((json: {
+            redirect_url?: string
+        } | null) => {
+            if(json && json.redirect_url) {
+                if (req.auth && req.auth.user) {
+                    fetch(json.redirect_url, {
+                        body: JSON.stringify({ username: req.auth.user.name, image: req.auth.user.image, status: 200 }),
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        method: "POST"
+                    })
+                    return NextResponse.json(req.auth)
+                } else {
+                    fetch(json.redirect_url, {
+                        body: JSON.stringify({ username: "", image: "", status: 401 }),
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        method: "POST",
+                    })
+                    return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
+                }
+            } else {
+                return NextResponse.json({ message: "Invalid request body" }, { status: 401 });
+            }
+        }).catch(() => {
+            return NextResponse.json({ message: "Invalid request body" }, { status: 401 });
+        })
+    })(req, ctx)
+}
